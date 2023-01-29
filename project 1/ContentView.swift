7044947c6fca8d92d8a1c068605b3c9aff5d7d4a
RavinduTharanga <ai4helab@gmail.com>
2023-01-29 15:52:44 -0500

    2

--- project 1/ContentView.swift
+++ project 1/ContentView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+   @State var tapCount = 0
+
     var body: some View {
+        NavigationView{
+    
         
         Form {
             Section{
-            
+                Button("Tap Count: \(tapCount)") {
+                                tapCount += 1
+                
+                }
+                
             Text("Hello, world!")
             }
             Text("Hello, world!")
@@ -25,6 +35,7 @@ struct ContentView: View {
             Text("Hello, world!")
         }
             
+    }
     }
 }
 
