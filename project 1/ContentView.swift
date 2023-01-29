de1b084f5d0c7937b3abedb8021578e4f6d73aaa
RavinduTharanga <ai4helab@gmail.com>
2023-01-29 13:37:41 -0500

    Initial Commit

---
+++ project 1/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  project 1
+//
+//  Created by MyMac on 1/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Form {
+            Section{
+            
+            Text("Hello, world!")
+            }
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project 1/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  project 1
+//
+//  Created by MyMac on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
